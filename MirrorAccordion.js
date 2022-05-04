d9e81c2189278ce8e69ad6a8e33d42b69426d1c1
Vishu Kalier <95927774+VishuKalier2003@users.noreply.github.com>
2022-05-04 13:25:25 +0530

    Add files via upload

---
+++ MirrorAccordion.js
@@ -0,0 +1,80 @@
+const q1 = document.getElementById('q1');
+const ar1 = document.getElementById('ar1');
+const a1 = document.getElementById('a1');
+q1.addEventListener('click', function() {
+    q1.classList.toggle('query-remove');
+    ar1.classList.toggle('query-area-remove');
+    a1.classList.toggle('appear');
+});
+
+const q2 = document.getElementById('q2');
+const ar2 = document.getElementById('ar2');
+const a2 = document.getElementById('a2');
+q2.addEventListener('click', function() {
+    q2.classList.toggle('query-remove');
+    ar2.classList.toggle('query-area-remove');
+    a2.classList.toggle('appear');
+});
+
+const q3 = document.getElementById('q3');
+const ar3 = document.getElementById('ar3');
+const a3 = document.getElementById('a3');
+q3.addEventListener('click', function() {
+    q3.classList.toggle('query-remove');
+    ar3.classList.toggle('query-area-remove');
+    a3.classList.toggle('appear');
+});
+
+const q4 = document.getElementById('q4');
+const ar4 = document.getElementById('ar4');
+const a4 = document.getElementById('a4');
+q4.addEventListener('click', function() {
+    q4.classList.toggle('query-remove');
+    ar4.classList.toggle('query-area-remove');
+    a4.classList.toggle('appear');
+});
+
+const q5 = document.getElementById('q5');
+const ar5 = document.getElementById('ar5');
+const a5 = document.getElementById('a5');
+q5.addEventListener('click', function() {
+    q5.classList.toggle('query-remove');
+    ar5.classList.toggle('query-area-remove');
+    a5.classList.toggle('appear');
+});
+
+const q6 = document.getElementById('q6');
+const ar6 = document.getElementById('ar6');
+const a6 = document.getElementById('a6');
+q6.addEventListener('click', function() {
+    q6.classList.toggle('query-remove');
+    ar6.classList.toggle('query-area-remove');
+    a6.classList.toggle('appear');
+});
+
+const q7 = document.getElementById('q7');
+const ar7 = document.getElementById('ar7');
+const a7 = document.getElementById('a7');
+q7.addEventListener('click', function() {
+    q7.classList.toggle('query-remove');
+    ar7.classList.toggle('query-area-remove');
+    a7.classList.toggle('appear');
+});
+
+const q8 = document.getElementById('q8');
+const ar8 = document.getElementById('ar8');
+const a8 = document.getElementById('a8');
+q8.addEventListener('click', function() {
+    q8.classList.toggle('query-remove');
+    ar8.classList.toggle('query-area-remove');
+    a8.classList.toggle('appear');
+});
+
+const q9 = document.getElementById('q9');
+const ar9 = document.getElementById('ar9');
+const a9 = document.getElementById('a9');
+q9.addEventListener('click', function() {
+    q9.classList.toggle('query-remove');
+    ar9.classList.toggle('query-area-remove');
+    a9.classList.toggle('appear');
+});
